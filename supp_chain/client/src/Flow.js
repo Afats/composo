72974ec9d23ed0c5eeee41e4298eec47f8645805
Afats <mmdohad@gmail.com>
2022-10-19 13:25:35 +1100

    flow template

--- supp_chain/client/src/Flow.js
+++ supp_chain/client/src/Flow.js
@@ -4,15 +4,24 @@ import 'reactflow/dist/style.css';
 
 const nodes = [
     {
-        id: '1',
-        position: { x: 0, y: 0 },
+      id: '1',
+      position: { x: 0, y: 0 },
+      data: { label: 'Hello' },
+      type: 'input',
+    },
+    {
+      id: '2',
+      position: { x: 100, y: 100 },
+      data: { label: 'World' },
     },
 ];
+
+  const edges = [{ id: '1-2', source: '1', target: '2', label: 'to the', type: 'bezier', animated: true }];
   
 function Flow() {
     return (
         <div style={{padding: '50px', height:'80%', width:'80%', position:'relative'}}>
-        <ReactFlow nodes={nodes}>
+        <ReactFlow nodes={nodes} edges={edges}>
             <Background />
             <Controls />
         </ReactFlow>
